3760976d93fc2cefd115c239d3c47a7fe029d199
lucasokada <lucas.okada@unesp.br>
2023-02-28 11:01:17 -0300

    feat: create common Entity and Agregate Root classes

---
+++ food-ordering-system/common/common-domain/src/main/java/org/food/ordering/system/domain/entity/AggregateRoot.java
@@ -0,0 +1,7 @@
+package org.food.ordering.system.domain.entity;
+
+//Quando olhar para o código, vai ser fácil
+//identificar qual é entidade e qual é agregado
+public abstract class AggregateRoot<ID> extends BaseEntity<ID> {
+
+}
